ff95161150cb3177ed461e9872a96530e4302fc9
Sebastian Bruckner <Sebruck@users.noreply.github.com>
2018-03-18 18:57:42 +0100

    Add doc to public api

--- core/src/main/scala/com/github/sebruck/opencensus/Tracing.scala
+++ core/src/main/scala/com/github/sebruck/opencensus/Tracing.scala
@@ -14,17 +14,41 @@ import io.opencensus.trace.{
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
 
-object Tracing extends LazyLogging {
-  private val config = loadConfigOrThrow[Config]("opencensus-scala")
-
-  if (config.stackdriver.enabled) {
-    Stackdriver.init(config.stackdriver)
-  }
-
+trait Tracing {
   private val tracer       = OpencensusTracing.getTracer
   private val unknownError = (_: Throwable) => Status.UNKNOWN
   private def ok[T]        = (_: T) => Status.OK
 
+  /**
+    * Starts a root span
+    */
+  def startSpan(name: String): Span = buildSpan(tracer.spanBuilder(name))
+
+  /**
+    * Starts a child span of the given parent
+    */
+  def startChildSpan(name: String, parent: Span): Span =
+    buildSpan(tracer.spanBuilderWithExplicitParent(name, parent))
+
+  /**
+    * Ends the span with the given status
+    */
+  def endSpan(span: Span, status: Status): Unit =
+    span.end(EndSpanOptions.builder().setStatus(status).build())
+
+  /**
+    * Starts a new child span of the given parent span before executing the given function.
+    *
+    * When the Future which is returned by the provided function completes successfully, the span will be ended
+    * with the status returned by `successStatus` otherwise with the status returned by `failureStatus`.
+    *
+    * @param name the name of the created span
+    * @param parentSpan the parent span
+    * @param successStatus function defining the status with which the Span will be ended in case of success
+    * @param failureStatus function defining the status with which the Span will be ended in case of failure
+    * @return an unary function which parameter is the action which should be traced. The newly created span is given
+    *         as a parameter in case it is needed as parent reference for further spans.
+    */
   def traceChild[T](name: String,
                     parentSpan: Span,
                     successStatus: T => Status = ok,
@@ -32,19 +56,23 @@ object Tracing extends LazyLogging {
       implicit ec: ExecutionContext): (Span => Future[T]) => Future[T] =
     traceSpan(startChildSpan(name, parentSpan), successStatus, failureStatus)
 
+  /**
+    * Starts a new root span before executing the given function.
+    *
+    * When the Future which is returned by the provided function completes successfully, the span will be ended
+    * with the status returned by `successStatus` otherwise with the status returned by `failureStatus`.
+    *
+    * @param name the name of the created span
+    * @param successStatus function defining the status with which the Span will be ended in case of success
+    * @param failureStatus function defining the status with which the Span will be ended in case of failure
+    * @return an unary function which parameter is the action which should be traced. The newly created span is given
+    *         as a parameter in case it is needed as parent reference for further spans.
+    */
   def trace[T](name: String,
                successStatus: T => Status = ok,
                failureStatus: Throwable => Status = unknownError)(
       implicit ec: ExecutionContext): (Span => Future[T]) => Future[T] =
     traceSpan(startSpan(name), successStatus, failureStatus)
-
-  def startSpan(name: String): Span = buildSpan(tracer.spanBuilder(name))
-
-  def startChildSpan(name: String, parent: Span): Span =
-    buildSpan(tracer.spanBuilderWithExplicitParent(name, parent))
-
-  def endSpan(span: Span, status: Status): Unit =
-    span.end(EndSpanOptions.builder().setStatus(status).build())
 
   private def traceSpan[T](span: Span,
                            successStatus: T => Status,
@@ -64,5 +92,13 @@ object Tracing extends LazyLogging {
     builder
       .setSampler(Samplers.alwaysSample())
       .startSpan()
+  }
+}
+
+object Tracing extends LazyLogging {
+  private val config = loadConfigOrThrow[Config]("opencensus-scala")
+
+  if (config.stackdriver.enabled) {
+    Stackdriver.init(config.stackdriver)
   }
 }
