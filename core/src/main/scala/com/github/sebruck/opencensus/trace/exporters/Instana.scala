fce0f81b066e8ff247204ff89aeaa255714faa9d
Sebastian Bruckner <Sebruck@users.noreply.github.com>
2018-05-04 23:15:50 +0200

    Avoid premature loading of exporter specific classes, #29

--- core/src/main/scala/com/github/sebruck/opencensus/trace/exporters/Instana.scala
+++ core/src/main/scala/com/github/sebruck/opencensus/trace/exporters/Instana.scala
@@ -6,10 +6,9 @@ import io.opencensus.exporter.trace.instana.InstanaTraceExporter
 
 private[opencensus] object Instana extends LazyLogging {
 
-  def init(config: InstanaTraceExporterConfig): Unit =
-    if (config.enabled) {
-      logger.info(
-        s"Enabling InstanaTraceExporter with agent endpoint ${config.agentEndpoint}")
-      InstanaTraceExporter.createAndRegister(config.agentEndpoint)
-    }
+  def init(config: InstanaTraceExporterConfig): Unit = {
+    logger.info(
+      s"Enabling InstanaTraceExporter with agent endpoint ${config.agentEndpoint}")
+    InstanaTraceExporter.createAndRegister(config.agentEndpoint)
+  }
 }
