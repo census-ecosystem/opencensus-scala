7a502d3d2597e5e9d920efce79fc92d1c1c9f361
Sebastian Bruckner <Sebruck@users.noreply.github.com>
2018-07-20 14:40:41 +0200

    Add service name and version to the span as attribute, resolves #26

---
+++ http/src/test/scala/io/opencensus/scala/http/ServiceAttributesSpec.scala
@@ -0,0 +1,35 @@
+package io.opencensus.scala.http
+
+import io.opencensus.scala.http.ServiceAttributes._
+import io.opencensus.scala.http.testSuite.MockSpan
+import io.opencensus.trace.AttributeValue._
+import org.scalatest.{FlatSpec, Matchers}
+
+class ServiceAttributesSpec extends FlatSpec with Matchers {
+
+  val strValue = stringAttributeValue _
+
+  behavior of "setAttributesForService"
+
+  it should "set the service name only" in {
+    val span = new MockSpan("", None)
+    setAttributesForService(span, ServiceData("myservice"))
+
+    span.attributes shouldBe Map("service.name" -> strValue("myservice"))
+  }
+
+  it should "set the version only" in {
+    val span = new MockSpan("", None)
+    setAttributesForService(span, ServiceData().setVersion("myversion"))
+
+    span.attributes shouldBe Map("service.version" -> strValue("myversion"))
+  }
+
+  it should "set all attributes" in {
+    val span = new MockSpan("", None)
+    setAttributesForService(span, ServiceData("myservice", "myversion"))
+
+    span.attributes shouldBe Map("service.name"    -> strValue("myservice"),
+                                 "service.version" -> strValue("myversion"))
+  }
+}
