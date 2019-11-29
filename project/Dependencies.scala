e639d98702db2b14112b8160b890b1175e423037
Scala Steward <me@scala-steward.org>
2019-11-29 12:09:43 +0100

    Update http4s-client, http4s-dsl to 0.21.0-M6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val ScalaLoggingVersion = "3.9.2"
   val AkkaHttpVersion     = "10.1.10"
   val AkkaVersion         = "2.5.26"
-  val Http4sVersion       = "0.21.0-M5"
+  val Http4sVersion       = "0.21.0-M6"
   val Elastic4sVersion    = "7.3.1"
   val CatsVersion         = "2.0.0"
   val DoobieVersion       = "0.8.4"
